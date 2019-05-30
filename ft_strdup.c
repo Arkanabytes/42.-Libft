f64403fc70013ce6a3563babb45a7ed99f3f0a6f
Consuelo alejandra Pinto toro <copinto-@e1z3r5p6.42.us.org>
2019-05-30 15:18:24 -0700

    new

---
+++ ft_strdup.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: copinto- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/01 16:11:14 by copinto-          #+#    #+#             */
+/*   Updated: 2019/05/22 21:56:49 by copinto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char			*ft_strdup(const char *src);
+{
+	char		*s;
+	int			len;
+
+	while(src[len])
+		++len;
+	if(!(s = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	s[len] = '\0';
+	{
+		s[len] = src[len];
+		len--;
+	}
+	return (s);
+}
